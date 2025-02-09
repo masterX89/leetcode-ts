87c3567afc66978678d95b29c02d21f559647ee3
Hankai Xia <xhk1995@gmail.com>
2025-02-09 16:17:45 +0800

    feat(0001): add two more solutions

--- src/solutions/0001_twoSum.ts
+++ src/solutions/0001_twoSum.ts
@@ -5,7 +5,34 @@ function twoSum1(nums: number[], target: number): number[] {
         return [i, j]
     }
   }
-  return []
+  return [-1, -1]
 }
 
-export { twoSum1 }
+function twoSum2(nums: number[], target: number): number[] {
+  const combined = nums.map((val, index) => ({ val, index }))
+  combined.sort((a, b) => a.val - b.val)
+  let l = 0
+  let r = combined.length - 1
+  while (l < r) {
+    if (combined[l].val + combined[r].val === target) {
+      return [combined[l].index, combined[r].index]
+    }
+    while (l < r && combined[l].val + combined[r].val > target) r--
+    while (l < r && combined[l].val + combined[r].val < target) l++
+  }
+  return [-1, -1]
+}
+
+function twoSum3(nums: number[], target: number): number[] {
+  const map: Record<number, number> = {}
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i]
+    if (Object.hasOwn(map, target - num)) {
+      return [i, map[target - num]]
+    }
+    map[num] = i
+  }
+  return [-1, -1]
+}
+
+export { twoSum1, twoSum2, twoSum3 }
--- src/tests/001_twoSum.test.ts
+++ src/tests/001_twoSum.test.ts
@@ -1,10 +1,26 @@
 import { describe, expect, it } from 'vitest'
-import { twoSum1 } from '../solutions/0001_twoSum'
+import { twoSum1, twoSum2, twoSum3 } from '../solutions/0001_twoSum'
+
+const testCases = [
+  { nums: [2, 7, 11, 15], target: 9, expected: [0, 1] },
+  { nums: [3, 2, 4], target: 6, expected: [1, 2] },
+  { nums: [3, 3], target: 6, expected: [0, 1] },
+]
+
+const solutions = [
+  { name: 'twoSum1', fn: twoSum1 },
+  { name: 'twoSum2', fn: twoSum2 },
+  { name: 'twoSum3', fn: twoSum3 },
+]
 
 describe('twoSum', () => {
-  it('should return the index', () => {
-    expect(twoSum1([2, 7, 11, 15], 9)).toEqual([0, 1])
-    expect(twoSum1([3, 2, 4], 6)).toEqual([1, 2])
-    expect(twoSum1([3, 3], 6)).toEqual([0, 1])
+  solutions.forEach(({ name, fn }) => {
+    describe(name, () => {
+      testCases.forEach(({ nums, target, expected }) => {
+        it(`${name} should return [${expected}]`, () => {
+          expect(fn(nums, target).sort((a, b) => a - b)).toEqual(expected)
+        })
+      })
+    })
   })
 })
