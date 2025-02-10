eaf3ae9dac6a5031917bfd9ce5913dc8d80678b2
Hankai Xia <xhk1995@gmail.com>
2025-02-10 11:34:54 +0800

    style(0374): update code style

--- src/solutions/0374_guessNumber.ts
+++ src/solutions/0374_guessNumber.ts
@@ -6,10 +6,13 @@ function guessNumber(n: number): number {
   // [..., r, l, ...]
   while (l <= r) {
     const mid = l + ((r - l) >> 1)
-    if (guess(mid) === 0) return mid
-    else if (guess(mid) === -1){
-      r = mid -1
-    }else if (guess(mid) === 1) {
+    if (guess(mid) === 0) {
+      return mid
+    }
+    else if (guess(mid) === -1) {
+      r = mid - 1
+    }
+    else if (guess(mid) === 1) {
       l = mid + 1
     }
   }
