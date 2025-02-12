b60204a7939fe6ec9b23dfd5e2ce7e171cd91b1a
Hankai Xia <xhk1995@gmail.com>
2025-02-12 19:11:13 +0800

    test(0374): update create function factory

--- src/tests/0374_guessNumber.test.ts
+++ src/tests/0374_guessNumber.test.ts
@@ -1,7 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { guessNumber } from '../solutions/0374_guessNumber'
 
-let pick: number = 0
+const testCases = [
+  { n: 10, pick: 6, expected: 6 },
+  { n: 1, pick: 1, expected: 1 },
+  { n: 2, pick: 1, expected: 1 },
+]
 /**
  * Forward declaration of guess API.
  * @param {number} num   your guess
@@ -10,28 +14,30 @@ let pick: number = 0
  *          otherwise return 0
  * var guess = function(num) {}
  */
-const guess = function (num: number): number {
-  if (num > pick) {
-    return -1
+
+function createGuess(pick: number) {
+  return (num: number): number => {
+    if (num > pick) {
+      return -1
+    }
+    else if (num < pick) {
+      return 1
+    }
+    return 0
   }
-  else if (num < pick) {
-    return 1
-  }
-  return 0
 }
 
+let guessRef = (_num: number): number => {
+  throw new Error('guess function is not implemented')
+}
+
+const guess = (num: number): number => guessRef(num)
 describe('guess Number', () => {
-  it('[1, 10] pick 6', () => {
-    pick = 6
-    expect(guessNumber(10)).toBe(pick)
-  })
-  it('[1, 1] pick 1', () => {
-    pick = 1
-    expect(guessNumber(1)).toBe(pick)
-  })
-  it('[1, 2] pick 1', () => {
-    pick = 1
-    expect(guessNumber(2)).toBe(pick)
+  testCases.forEach(({ n, pick, expected }) => {
+    it(`${n} pick ${pick} should return ${expected}`, () => {
+      guessRef = createGuess(pick)
+      expect(guessNumber(n)).toBe(pick)
+    })
   })
 })
 
