e03871efc5fc53182768de6e6a3c7c760465620d
Hankai Xia <xhk1995@gmail.com>
2025-02-12 09:53:41 +0800

    feat(0154): update solution with finding pivotIndex

--- src/solutions/0154_findMinRotated_II.ts
+++ src/solutions/0154_findMinRotated_II.ts
@@ -1,5 +1,13 @@
-function findMin(nums: number[]): number {
-  // if all of the numbers are the same or mid is actually mid, the worst situation is O(n)
+/**
+ * This function was an experimental approach to find the pivot index.
+ * It does not correctly handle all cases (e.g. [1, 1, 20, 1] should expect 3 but return 0)
+ * and is kept here for analysis and documentation purposes.
+ *
+ * @deprecated Use getPivotIndex instead.
+ */
+function getMinIndex(nums: number[]): number {
+  // if all of the numbers are the same
+  // or mid is actually mid, the worst situation is O(n)
   let l = 0
   let r = nums.length - 1
   // [..., r, l, ...]
@@ -18,7 +26,34 @@ function findMin(nums: number[]): number {
     }
   }
   // l must be less than nums.length
+  // but may not be the pivot index
+  // e.g. [1, 1, 10, 1], l is 0, but the pivot is 3
+  return l
+}
+
+function getPivotIndex(nums: number[]): number {
+  let l = 0
+  let r = nums.length - 1
+  while (r > 0 && nums[r] === nums[0]) {
+    r--
+  }
+  // [..., r, l, ...]
+  while (l <= r) {
+    const mid = l + ((r - l) >> 1)
+    if (nums[mid] < nums[0]) {
+      r = mid - 1
+    }
+    else {
+      l = mid + 1
+    }
+  }
+  // if not rotated, l will be nums.length and return 0
+  return l !== nums.length ? l : 0
+}
+
+function findMin(nums: number[]): number {
+  const l = getPivotIndex(nums)
   return nums[l]
 };
 
-export { findMin }
+export { findMin, getMinIndex, getPivotIndex }
--- src/tests/0154_findMinRotated_II.test.ts
+++ src/tests/0154_findMinRotated_II.test.ts
@@ -1,18 +1,20 @@
 import { describe, expect, it } from 'vitest'
-import { findMin } from '../solutions/0154_findMinRotated_II'
+import { findMin, getMinIndex, getPivotIndex } from '../solutions/0154_findMinRotated_II'
 
 const testCases = [
-  { nums: [3, 4, 5, 1, 2], expected: 1 },
-  { nums: [4, 5, 6, 7, 0, 1, 2], expected: 0 },
-  { nums: [11, 13, 15, 17], expected: 11 },
-  { nums: [1, 3, 5], expected: 1 },
-  { nums: [2, 2, 2, 0, 1], expected: 0 },
-  { nums: [10, 1, 10, 10, 10], expected: 1 },
+  { nums: [3, 4, 5, 1, 2], expected: 1, pivot: 3 },
+  { nums: [4, 5, 6, 7, 0, 1, 2], expected: 0, pivot: 4 },
+  { nums: [11, 13, 15, 17], expected: 11, pivot: 0 },
+  { nums: [1, 3, 5], expected: 1, pivot: 0 },
+  { nums: [2, 2, 2, 0, 1], expected: 0, pivot: 3 },
+  { nums: [10, 1, 10, 10, 10], expected: 1, pivot: 1 },
+  { nums: [1, 1, 20, 1], expected: 1, pivot: 3 },
 ]
 
 describe('find the minium number in a rotated array', () => {
-  testCases.forEach(({ nums, expected }) => {
-    it(`should return ${expected}`, () => {
+  testCases.forEach(({ nums, expected, pivot }) => {
+    it(`${nums} pivot should be ${pivot} and return ${expected}`, () => {
+      expect(getPivotIndex(nums)).toBe(pivot)
       expect(findMin(nums)).toBe(expected)
     })
   })
