47ca746df30901e0716849477135fce24b05794e
Hankai Xia <xhk1995@gmail.com>
2025-03-13 10:52:07 +0800

    feat(0912): update quick sort solution

--- src/solutions/0912_sortArray.ts
+++ src/solutions/0912_sortArray.ts
@@ -14,16 +14,60 @@ const SortAlgorithms = {
       }
       return resArr.concat(leftArr.slice(i), rightArr.slice(j))
     }
-    const mergeSort = (nums: number[]): number[] => {
+    const sort = (nums: number[]): number[] => {
       if (nums.length <= 1) {
         return nums
       }
       const mid = Math.floor(nums.length / 2)
-      const leftArr = mergeSort(nums.slice(0, mid))
-      const rightArr = mergeSort(nums.slice(mid))
+      const leftArr = sort(nums.slice(0, mid))
+      const rightArr = sort(nums.slice(mid))
       return merge(leftArr, rightArr)
     }
-    return mergeSort(nums)
+    return sort(nums)
+  },
+  quickSort: function sortArray(nums: number[]): number[] {
+    // [1,2,3,4,5] => not balance
+    const shuffle = (nums: number[]) => {
+      for (let i = nums.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[nums[i], nums[j]] = [nums[j], nums[i]]
+      }
+    }
+    const partition = (nums: number[], left: number, right: number): number => {
+      const pivot = nums[left]
+      // i: nums[i] >= pivot, j: nums[j] <= pivot
+      // ..., j(<=pivot), i(>=pivot),...
+      let i = left + 1
+      let j = right
+      while (i <= j) {
+        // nums[i] < pivot faster 100x than <= ex. [6,6,6,6] j will be right position
+        // [left,right), pivot, [] => not balance
+        while (i <= j && nums[i] < pivot) {
+          i++
+        }
+        while (i <= j && nums[j] > pivot) {
+          j--
+        }
+        if (i <= j) {
+          ;[nums[i], nums[j]] = [nums[j], nums[i]]
+          i++
+          j--
+        }
+      }
+      ;[nums[left], nums[j]] = [nums[j], nums[left]]
+      return j
+    }
+    const sort = (nums: number[], left: number, right: number) => {
+      if (left >= right) {
+        return
+      }
+      const pivot = partition(nums, left, right)
+      sort(nums, left, pivot - 1)
+      sort(nums, pivot + 1, right)
+    }
+    shuffle(nums)
+    sort(nums, 0, nums.length - 1)
+    return nums
   },
 }
 
--- src/tests/0912_sortArray.test.ts
+++ src/tests/0912_sortArray.test.ts
@@ -5,12 +5,19 @@ const testCases = [
   { nums: [5, 2, 3, 1], expected: [1, 2, 3, 5] },
   { nums: [5, 1, 1, 2, 0, 0], expected: [0, 0, 1, 1, 2, 5] },
 ]
+const solutions = [
+  // { name: 'mergeSort', fn: SortAlgorithms.mergeSort },
+  { name: 'quickSort', fn: SortAlgorithms.quickSort },
+]
 
 describe('merge sort', () => {
-  const { mergeSort } = SortAlgorithms
-  testCases.forEach(({ nums, expected }) => {
-    it(`mergeSort ${nums} should return ${expected}`, () => {
-      expect(mergeSort(nums)).toEqual(expected)
+  solutions.forEach(({ name, fn }) => {
+    describe(name, () => {
+      testCases.forEach(({ nums, expected }) => {
+        it(`mergeSort ${nums} should return ${expected}`, () => {
+          expect(fn(nums)).toEqual(expected)
+        })
+      })
     })
   })
 })
