5e1c3ecf5d4b38d82f5b72aee28a0732f487e568
Hankai Xia <xhk1995@gmail.com>
2025-06-03 14:52:05 +0800

    chore: eslint add cjs console log

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,13 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu(
+  {
+    // Configures for antfu's config
+  },
+  {
+    files: ['**/*.cjs'],
+    rules: {
+      'no-console': 'off',
+    },
+  },
+)
