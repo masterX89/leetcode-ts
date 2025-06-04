30779c482aa39a7799a070ade9bc4addefe626ea
Hankai Xia <xhk1995@gmail.com>
2025-06-04 14:50:27 +0800

    feat: add typical90_027 solution

---
+++ src/solutions/atcoder/typical90_027_SignUpRequests.cjs
@@ -0,0 +1,25 @@
+function main(stdin) {
+  // TODO: add tests
+  const lines = stdin.trim().split('\n')
+  const n = Number(lines[0])
+  const logins = lines.slice(1)
+
+  const res = []
+  const userNames = new Set()
+  for (let i = 0; i < n; i++) {
+    if (!userNames.has(logins[i])) {
+      userNames.add(logins[i])
+      res.push(i + 1)
+    }
+  }
+
+  return res.join('\n')
+}
+
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+
+module.exports = { main }
