7a3713dabdb0110a336f18d4a58f04f8eeeda619
Hankai Xia <xhk1995@gmail.com>
2025-06-26 19:59:19 +0800

    feat: update ABC_395 AB solution

---
+++ src/solutions/atcoder/ABC_391_400/ABC_395/B.cjs
@@ -0,0 +1,60 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const n = next()
+  const mass = xArray([n, n], () => '.')
+  const res = []
+  for (let l = 0; l <= Math.floor(n / 2); l += 2) {
+    const r = n - l - 1
+    for (let j = l; j <= r; j++) {
+      mass[l][j] = '#'
+      mass[r][j] = '#'
+    }
+    for (let i = l; i <= r; i++) {
+      mass[i][l] = '#'
+      mass[i][r] = '#'
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    res.push(mass[i].join(''))
+  }
+  return res.join('\n')
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+
+module.exports = { main }
