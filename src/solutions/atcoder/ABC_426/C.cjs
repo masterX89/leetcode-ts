225b7fb1551b849b20fa5f85347025847a14604f
Hankai Xia <xhk1995@gmail.com>
2025-10-22 14:25:27 +0900

    feat(ABC_426): solution C

---
+++ src/solutions/atcoder/ABC_426/C.cjs
@@ -0,0 +1,56 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const [n, q] = nexts(2)
+  const version2Cnt = Array.from({ length: n + 1 }, () => 1)
+  const res = []
+  let r = 1
+  for (let i = 0; i < q; i++) {
+    const [x, y] = nexts(2)
+    let ans = 0
+    while (r <= x) {
+      ans += version2Cnt[r]
+      version2Cnt[r] = 0
+      r++
+    }
+    version2Cnt[y] += ans
+    res.push(ans)
+  }
+  return res.join('\n')
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+module.exports = { main }
