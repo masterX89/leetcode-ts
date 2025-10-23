d434743aa262f62dd074655ce4a62ca02b1c3ff0
Hankai Xia <xhk1995@gmail.com>
2025-10-23 09:43:55 +0900

    feat(ABC_425): solution ABC

---
+++ src/solutions/atcoder/ABC_425/B.cjs
@@ -0,0 +1,63 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const n = next()
+  const nums = nexts(n)
+  const set = new Set(Array.from({ length: n }, (_, i) => i + 1))
+  for (const num of nums) {
+    if (num !== -1) {
+      set.delete(num)
+    }
+  }
+  const res = []
+  const minus = Array.from(set)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === -1) {
+      nums[i] = minus.pop()
+    }
+  }
+  if (minus.length === 0) {
+    res.push('Yes')
+    res.push(nums.join(' '))
+  }
+  else {
+    res.push('No')
+  }
+  return res.join('\n')
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+module.exports = { main }
---
+++ src/solutions/atcoder/ABC_425/C.cjs
@@ -0,0 +1,71 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const [n, q] = nexts(2)
+  const a = nexts(n)
+  const prefix = [0]
+  for (const num of a) {
+    prefix.push(prefix[prefix.length - 1] + num)
+  }
+  let s = 0
+  const res = []
+  for (let i = 0; i < q; i++) {
+    const t = next()
+    if (t === 1) {
+      const c = next()
+      s = (s + c) % n
+    }
+    else if (t === 2) {
+      let [l, r] = nexts(2)
+      l--
+      r--
+      l = (s + l) % n
+      r = (s + r) % n
+      let sum = 0
+      if (l <= r) {
+        sum = prefix[r + 1] - prefix[l]
+      }
+      else {
+        sum = (prefix[r + 1] - prefix[0]) + (prefix[n] - prefix[l])
+      }
+      res.push(sum)
+    }
+  }
+  return res.join('\n')
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+module.exports = { main }
