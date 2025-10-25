4890cafb38c23d395518059735f057ec44786fcc
Hankai Xia <xhk1995@gmail.com>
2025-10-25 22:50:43 +0900

    feat(ABC_429): solution ABC

---
+++ src/solutions/atcoder/ABC_429/C.cjs
@@ -0,0 +1,58 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const n = next()
+  const a = nexts(n)
+  const freq = new Map()
+  for (const num of a) {
+    if (freq.has(num)) {
+      freq.set(num, freq.get(num) + 1)
+    }
+    else {
+      freq.set(num, 1)
+    }
+  }
+  let res = 0n
+  freq.forEach((value) => {
+    if (value >= 2) {
+      res += (BigInt(n) - BigInt(value)) * BigInt(value) * (BigInt(value) - 1n) / 2n
+    }
+  })
+  return res.toString()
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+module.exports = { main }
