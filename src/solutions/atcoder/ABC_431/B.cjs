bebcbed8ec189f0226b2d0614cf5c2300f3ec7f0
Hankai Xia <xhk1995@gmail.com>
2025-11-15 20:41:49 +0900

    feat(ABC_431): solution ABC

---
+++ src/solutions/atcoder/ABC_431/B.cjs
@@ -0,0 +1,53 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const x = next()
+  const n = next()
+  const w = nexts(n)
+  const q = next()
+  const res = [x]
+  const isFill = Array.from({ length: n }, () => false)
+  for (let i = 0; i < q; i++) {
+    const p = next()
+    const now = isFill[p - 1] ? res[res.length - 1] - w[p - 1] : res[res.length - 1] + w[p - 1]
+    res.push(now)
+    isFill[p - 1] = !isFill[p - 1]
+  }
+  return res.slice(1).join('\n')
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+module.exports = { main }
