e16151b3863bcc639a58a9f5cfe53b05c45ce088
Hankai Xia <xhk1995@gmail.com>
2026-04-04 22:02:25 +0900

    feat(ABC_452): solution ABC

---
+++ src/solutions/atcoder/ABC_452/B.cjs
@@ -0,0 +1,57 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const [h, w] = nexts(2)
+  const map = Array.from({ length: h }, () => Array.from({ length: w }, () => '.'))
+  for (let i = 0; i < h; i++) {
+    for (let j = 0; j < w; j++) {
+      if (i === 0 || i === h - 1) {
+        map[i][j] = '#'
+      }
+      if (j === 0 || j === w - 1) {
+        map[i][j] = '#'
+      }
+    }
+  }
+  const res = []
+  for (let i = 0; i < map.length; i++) {
+    res.push(map[i].join(''))
+  }
+  return res.join('\n')
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+module.exports = { main }
