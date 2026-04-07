59c2e9a9a1c63ec1321d500263d0ffa334bd8fae
Hankai Xia <xhk1995@gmail.com>
2026-04-07 17:32:08 +0900

    feat(ADTE_260407): add ABC solution

---
+++ src/solutions/atcoder/ADT/260407/EZ1/D.cjs
@@ -0,0 +1,57 @@
+function main(stdin) {
+  // eslint-disable-next-line unused-imports/no-unused-vars, no-unused-vars
+  const { next, nextstr, nextbig, nexts, nextssort, nextm, xArray } = makeInputReader(stdin)
+  const [n, q] = nexts(2)
+  const members = Array.from({ length: n }, () => Array.from({ length: 2 }, () => 0))
+  const res = []
+  for (let i = 0; i < q; i++) {
+    let [type, x] = nexts(2)
+    x--
+    if (type === 1) {
+      members[x][0]++
+    }
+    else if (type === 2) {
+      members[x][1]++
+    }
+    else if (type === 3) {
+      res.push(members[x][0] === 2 || members[x][1] === 1 ? 'Yes' : 'No')
+    }
+  }
+  return res.join('\n')
+}
+function makeInputReader(input) {
+  const cin = input.trim().split(/ |\n/)
+  let cid = 0
+  function createNDArray(shape, fillFn) {
+    if (shape.length === 0)
+      return fillFn()
+    const [dim, ...rest] = shape
+    return Array.from({ length: dim }, () => createNDArray(rest, fillFn))
+  }
+  return {
+    next: () => Number(cin[cid++]),
+    nextstr: () => cin[cid++],
+    nextbig: () => BigInt(cin[cid++]),
+    nexts: (n, addNum = 0, parse = true) =>
+      parse ? cin.slice(cid, cid += n).map(s => Number(s) + addNum) : cin.slice(cid, cid += n),
+    nextssort: (n, parse = true, desc = false) =>
+      parse
+        ? cin.slice(cid, cid += n).map(Number).sort((a, b) => desc ? b - a : a - b)
+        : cin.slice(cid, cid += n).sort((a, b) => desc ? b.localeCompare(a) : a.localeCompare(b)),
+    nextm: (h, w, parse = true) => {
+      const res = []
+      for (let i = 0; i < h; i++) {
+        const row = cin.slice(cid, cid += w)
+        res.push(parse ? row.map(Number) : row)
+      }
+      return res
+    },
+    xArray: createNDArray,
+  }
+}
+if (require.main === module) {
+  const fs = require('node:fs')
+  const input = fs.readFileSync('/dev/stdin', 'utf8')
+  console.log(main(input))
+}
+module.exports = { main }
